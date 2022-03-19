69223957586f99223b937dbae4ad157687be7255
MartinPihrtJR <64539948+MartinPihrtJR@users.noreply.github.com>
2022-03-19 21:22:14 +0100

    Revert "new ordering system made"
    This reverts commit 22bcebb40ab98b03dce8f2bce66a2fb14f717df1.

--- app/src/main/java/cz/pihrtm/spseicanteen/ui/slideshow/SlideshowFragment.kt
+++ app/src/main/java/cz/pihrtm/spseicanteen/ui/slideshow/SlideshowFragment.kt
@@ -5,11 +5,12 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.*
+import android.widget.RadioButton
+import android.widget.RadioGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import cz.pihrtm.spseicanteen.R
-
 
 class SlideshowFragment : Fragment() {
 
@@ -20,37 +21,39 @@ class SlideshowFragment : Fragment() {
             container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
-        userViewModel = ViewModelProvider(this)[SlideshowViewModel::class.java]
+        userViewModel = ViewModelProvider(this).get(SlideshowViewModel::class.java)
         val view = inflater.inflate(R.layout.fragment_slideshow, container, false)
         val sharedPref = context?.getSharedPreferences("autoOrder", Context.MODE_PRIVATE)
-        val spinnerMo: Spinner = view.findViewById(R.id.spinnerMo)
+        val orderGroup: RadioGroup = view.findViewById(R.id.radioGroup)
         val orderDisabled: RadioButton = view.findViewById(R.id.autoOrder0)
         val order1: RadioButton = view.findViewById(R.id.autoOrder1)
-        val order2: RadioButton = view.findViewById(R.id.autoOrder2) //TODO smazat
+        val order2: RadioButton = view.findViewById(R.id.autoOrder2)
         val order3: RadioButton = view.findViewById(R.id.autoOrder3)
         //**************************************************************
-        // Create an ArrayAdapter using the string array and a default spinner layout
-        // Create an ArrayAdapter using the string array and a default spinner layout
-        val adapter = ArrayAdapter.createFromResource(
-            requireContext(),
-            R.array.spinnerItems, android.R.layout.simple_spinner_item
-        )
-        // Specify the layout to use when the list of choices appears
-        // Specify the layout to use when the list of choices appears
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-        // Apply the adapter to the spinner
-        // Apply the adapter to the spinner
-        spinnerMo.adapter = adapter
         //**********************************
-
+        when {
+            sharedPref?.getInt("mode",0)==0 -> {
+                orderGroup.check(R.id.autoOrder0)
+            }
+            sharedPref?.getInt("mode",0)==1 -> {
+                orderGroup.check(R.id.autoOrder1)
+            }
+            sharedPref?.getInt("mode",0)==2 -> {
+                orderGroup.check(R.id.autoOrder2)
+            }
+            sharedPref?.getInt("mode",0)==3 -> {
+                orderGroup.check(R.id.autoOrder3)
+            }
+        }
         orderDisabled.setOnClickListener {
-
+            sharedPref?.edit()?.putInt("mode",0)?.apply()
+            Toast.makeText(context,getString(R.string.autoOrderToast0),Toast.LENGTH_SHORT).show()
         }
         order1.setOnClickListener {
             sharedPref?.edit()?.putInt("mode",1)?.apply()
             Toast.makeText(context,getString(R.string.autoOrderToast1),Toast.LENGTH_SHORT).show()
         }
-        order2.setOnClickListener { //TODO smazat
+        order2.setOnClickListener {
             sharedPref?.edit()?.putInt("mode",2)?.apply()
             Toast.makeText(context,getString(R.string.autoOrderToast2),Toast.LENGTH_SHORT).show()
         }
@@ -59,59 +62,7 @@ class SlideshowFragment : Fragment() {
             Toast.makeText(context,getString(R.string.autoOrderToast3),Toast.LENGTH_SHORT).show()
         }
 
-        spinnerMo.onItemSelectedListener = object : AdapterView.OnItemSelectedListener { //TODO copy na zbyly dny
-            override fun onItemSelected(
-                parent: AdapterView<*>,
-                view: View,
-                position: Int,
-                id: Long
-            ) {
-                when (position) {
-                    0 -> {
-                        sharedPref?.edit()?.putInt("MO", 0)?.apply()
-                        Toast.makeText(
-                            context,
-                            getString(R.string.autoOrderToast0),
-                            Toast.LENGTH_SHORT
-                        ).show()
-                    }
-                    1 -> {
-                        sharedPref?.edit()?.putInt("MO", 1)?.apply()
-                        Toast.makeText(
-                            context,
-                            getString(R.string.autoOrderToast0),
-                            Toast.LENGTH_SHORT
-                        ).show()
-                    }
-                    2 -> {
-                        sharedPref?.edit()?.putInt("MO", 2)?.apply()
-                        Toast.makeText(
-                            context,
-                            getString(R.string.autoOrderToast0),
-                            Toast.LENGTH_SHORT
-                        ).show()
-                    }
-                    3 -> {
-                        sharedPref?.edit()?.putInt("MO", 3)?.apply()
-                        Toast.makeText(
-                            context,
-                            getString(R.string.autoOrderToast0),
-                            Toast.LENGTH_SHORT
-                        ).show()
-                    }
-                }
-            }
-
-            override fun onNothingSelected(parent: AdapterView<*>?) {
-
-                // sometimes you need nothing here
-            }
-        }
-        //TODO pro kazdej den se ulozi do promenne MO - FR a v orderFood se pak nacte pro kazdej den
-
-
 
         return view
     }
-
 }
