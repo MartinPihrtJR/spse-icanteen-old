a9a0512788d89d8dc9e64cdeef91408997dd635f
MartinPihrtJR <64539948+MartinPihrtJR@users.noreply.github.com>
2022-04-13 21:25:58 +0200

    update

--- app/src/main/java/cz/pihrtm/spseicanteen/adapter/OrderAdapter.kt
+++ app/src/main/java/cz/pihrtm/spseicanteen/adapter/OrderAdapter.kt
@@ -1,6 +1,7 @@
 package cz.pihrtm.spseicanteen.adapter
 
 
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -15,7 +16,7 @@ class OrderAdapter(val list: Array<FoodList?>) : RecyclerView.Adapter<OrderAdapt
     class ItemViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView)
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
-    val view = LayoutInflater.from(parent.context).inflate(R.layout.list_obed,parent,false)
+    val view = LayoutInflater.from(parent.context).inflate(R.layout.list_ordering,parent,false)
     return ItemViewHolder(view)
     }
 
@@ -28,17 +29,19 @@ class OrderAdapter(val list: Array<FoodList?>) : RecyclerView.Adapter<OrderAdapt
         val datum = view.findViewById<TextView>(R.id.ob_Datum)
         val polevka = view.findViewById<TextView>(R.id.ob_Polevka)
         val spinner = view.findViewById<Spinner>(R.id.ob_FoodSpinner)
-        datum.text = list[position]?.datum
-        polevka.text = list[position]?.polevka
+        Log.d("pos", position.toString())
+        datum.text = list[position]?.datum.toString()
+        polevka.text = list[position]?.polevka.toString()
         val context = datum.context
         val listObed: MutableList<String> = ArrayList()
+        listObed.add(context.getString(R.string.ordering_noOrdered))
         listObed.add(list[position]?.obed1.toString())
         listObed.add(list[position]?.obed2.toString())
         listObed.add(list[position]?.obed3.toString())
 
         val adp1: ArrayAdapter<String> = ArrayAdapter<String>(
             context,
-            android.R.layout.simple_list_item_1, listObed
+            android.R.layout.simple_spinner_item, listObed
         )
         adp1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
         spinner.adapter = adp1
--- app/src/main/java/cz/pihrtm/spseicanteen/ui/ordering/OrderingFragment.kt
+++ app/src/main/java/cz/pihrtm/spseicanteen/ui/ordering/OrderingFragment.kt
@@ -15,11 +15,13 @@ import cz.pihrtm.spseicanteen.R
 import cz.pihrtm.spseicanteen.adapter.OrderAdapter
 import cz.pihrtm.spseicanteen.model.FoodList
 import org.json.JSONArray
+import java.io.StringReader
 
 class OrderingFragment : Fragment() {
 
     private lateinit var userViewModel: OrderingViewModel
     private lateinit var recycler: RecyclerView
+    private lateinit var list: Array<FoodList?>
 
     override fun onCreateView(
             inflater: LayoutInflater,
@@ -32,25 +34,51 @@ class OrderingFragment : Fragment() {
         val scroll = savedInstanceState?.getInt("scrollOrder",0)?:0
         val nodata = view.findViewById<TextView>(R.id.noData)
 
+
         try {
-            var json = context?.openFileInput("foodlist.json")?.bufferedReader()?.readLines().toString() //read
-            json = json.subSequence(1, json.length - 1).toString()
-            val mainObject = JSONArray(json)
-            val delkajson = mainObject.length() - 1
-            val list: Array<FoodList?> = arrayOfNulls<FoodList>(delkajson+1)
-            for (i in 0..delkajson){
-                val obed = mainObject.getJSONObject(i)
-                val datum: String = obed.getString("datum")
-                val obed1: String = obed.getString("obed1")
-                val obed2: String = obed.getString("obed2")
-                val obed3: String = obed.getString("obed3")
-                val polevka: String = obed.getString("polevka1")
-                list[i] = FoodList(datum, obed1, obed2, obed3, polevka)
+            try {
+                var json = context?.openFileInput("foodlist.json")?.bufferedReader()?.readLines().toString() //read
+                json = json.subSequence(1, json.length - 1).toString()
+                val mainObject = JSONArray(json)
+                val delkajson = mainObject.length() - 1
+                list = arrayOfNulls<FoodList>(delkajson+1)
+                for (i in 0..delkajson){
+                    val obed = mainObject.getJSONObject(i)
+                    val datum: String = obed.getString("datum")
+                    val obed1: String = try {
+                        obed.getString("obed1")
+                    } catch (e: Exception){
+                        context?.getString(R.string.ordering_noOrdered).toString()
+                    }
+                    val obed2: String = try {
+                        obed.getString("obed2")
+                    } catch (e: Exception){
+                        context?.getString(R.string.ordering_noOrdered).toString()
+                    }
+                    val obed3: String = try {
+                        obed.getString("obed3")
+                    } catch (e: Exception){
+                        context?.getString(R.string.ordering_noOrdered).toString()
+                    }
+                    val polevka: String = try {
+                        obed.getString("polevka2")
+                    } catch (e: Exception){
+                        context?.getString(R.string.ordering_noOrdered).toString()
+                    }
+
+                    list[i] = FoodList(datum, obed1, obed2, obed3, polevka)
 
 
+                }
+
+                list.reverse()
+                if (list.isEmpty()) {
+                    nodata.visibility = View.VISIBLE
+                    recycler.visibility = View.GONE
+                }
+            } catch (e: Exception){
+                Log.d("OrderingJsonParse", e.toString())
             }
-
-            list.reverse()
             if (list.isEmpty()) {
                 nodata.visibility = View.VISIBLE
                 recycler.visibility = View.GONE
@@ -61,7 +89,7 @@ class OrderingFragment : Fragment() {
             recycler.addItemDecoration(DividerItemDecoration(context,LinearLayoutManager.VERTICAL))
             (recycler.layoutManager as LinearLayoutManager).scrollToPosition(scroll)
         } catch (e: Exception){
-
+            Log.d("OrderingStart", e.toString())
         }
 
         return view
